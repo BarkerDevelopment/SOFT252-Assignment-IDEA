2d44ea857ae2dfd475c2b6979a402e983c313f55
Max Barker <max@the-barkers.com>
2019-12-17 16:58:53 +0000

    New drug request
    NewDrug request allows doctors to request drugs that aren't in the system to be introduced to the system. However, if they request a drug that already exists, a DrugDuplicationError is thrown.

---
+++ src/soft252/model/request/NewDrugRequest.java
@@ -0,0 +1,131 @@
+package soft252.model.request;
+
+import soft252.exceptions.DrugDuplicateException;
+import soft252.model.drugs.Drug;
+import soft252.model.drugs.DrugRepository;
+import soft252.model.drugs.I_Treatment;
+import soft252.model.user.Doctor;
+import soft252.model.user.messaging.Message;
+
+import java.util.ArrayList;
+
+public class NewDrugRequest extends Request
+    implements I_Treatment {
+
+    private final Doctor _doctor;
+    private final String _name;
+    private String _description;
+    private ArrayList<String> _sideEffects;
+    private int _startingQty;
+
+    /**
+     *
+     * @param requester
+     * @param name
+     */
+    public NewDrugRequest(Doctor requester, String name) throws DrugDuplicateException {
+        if(DrugRepository.getInstance().contains(name)) throw new DrugDuplicateException();
+
+        _doctor = requester;
+        _name = name;
+        _sideEffects = new ArrayList<>();
+    }
+
+    /**
+     *
+     * @param requester
+     * @param name
+     * @param qty
+     */
+    public NewDrugRequest(Doctor requester, String name, int qty) throws DrugDuplicateException {
+        if(DrugRepository.getInstance().contains(name)) throw new DrugDuplicateException();
+        _doctor = requester;
+        _name = name;
+        _sideEffects = new ArrayList<>();
+        _startingQty = qty;
+    }
+
+    /**
+     * @return the _doctor variable. This is the user that requested the new drug.
+     */
+    public Doctor getDoctor() {
+        return _doctor;
+    }
+
+    /**
+     * @return the _name variable. This is the name of the new drug.
+     */
+    @Override
+    public String getName() {
+        return _name;
+    }
+
+    /**
+     * @return the _description variable.
+     */
+    @Override
+    public String getDescription() {
+        return _description;
+    }
+
+    /**
+     * @return the _sideEffects variable. This represents the known side effects of the drugs.
+     */
+    @Override
+    public ArrayList< String > getSideEffects() {
+        return _sideEffects;
+    }
+
+    /**
+     * @return
+     */
+    public int getStartingQty() {
+        return _startingQty;
+    }
+
+    /**
+     * @param description
+     */
+    public void setDescription(String description) {
+        _description = description;
+    }
+
+    /**
+     * @param sideEffects
+     */
+    public void setSideEffects(ArrayList< String > sideEffects) {
+        _sideEffects = sideEffects;
+    }
+
+    /**
+     * @param startingQty
+     */
+    public void setStartingQty(int startingQty) {
+        _startingQty = startingQty;
+    }
+
+    /**
+     * The action following request approval.
+     */
+    @Override
+    protected void approveAction() {
+        DrugRepository repository = DrugRepository.getInstance();
+
+        if(_startingQty == 0){
+            repository.add(new Drug(_name, _description, _sideEffects));
+
+        }else{
+            repository.add(new Drug(_name, _description, _sideEffects), _startingQty);
+        }
+
+        _doctor.addMessage(new Message(String.format("Your drug request for %s has been approved.", _name)));
+    }
+
+    /**
+     * The action following request denial.
+     */
+    @Override
+    protected void denyAction() {
+        _doctor.addMessage(new Message(String.format("Your drug request for %s has been denied.", _name)));
+    }
+}
